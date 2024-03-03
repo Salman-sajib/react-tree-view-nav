d184ac9bf26dd6b4be02054c4ea133e8d142941f
Salman-sajib <sfsajib@gmail.com>
2024-03-03 20:03:11 +0600

    rendered the childrens from the nav items

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,9 +1,15 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import MenuList from './MenuList';
 
 function MenuItem({ item }) {
   return (
     <li>
       <p> {item.label} </p>
+
+      {item && item.children && item.children.length > 0 ? (
+        <MenuList list={item.children} />
+      ) : null}
     </li>
   );
 }
