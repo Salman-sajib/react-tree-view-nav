34fd07692e9e7d5889f4615165b9c8cc0c1d6166
Salman-sajib <sfsajib@gmail.com>
2024-03-03 20:28:38 +0600

    added the click functionality depending the children to the nav items

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,13 +1,35 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 import MenuList from './MenuList';
 
 function MenuItem({ item }) {
+  const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
+
+  function handleToggleChildren(getCurrentLabel) {
+    setDisplayCurrentChildren({
+      ...displayCurrentChildren,
+      [getCurrentLabel]: !displayCurrentChildren[getCurrentLabel],
+    });
+  }
+
+  console.log(displayCurrentChildren);
+
   return (
     <li>
-      <p> {item.label} </p>
+      <div style={{ display: 'flex', alignItems: 'center', gap: '20px' }}>
+        <p> {item.label} </p>
+        {item && item.children && item.children.length > 0 ? (
+          <span onClick={() => handleToggleChildren(item.label)}>
+            {' '}
+            {displayCurrentChildren[item.label] ? '-' : '+'}{' '}
+          </span>
+        ) : null}
+      </div>
 
-      {item && item.children && item.children.length > 0 ? (
+      {item &&
+      item.children &&
+      item.children.length > 0 &&
+      displayCurrentChildren[item.label] ? (
         <MenuList list={item.children} />
       ) : null}
     </li>
